066d3d6f09c0a7de7936f04b403861d67b90e24a
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-01-31 19:18:23 +0100

    Config path exception handling + reading data from mongo[FXRATE]

--- spark/batch/src/main/scala/com/tradesys/jobs/fx/ForeignExchangeDailyRates.scala
+++ spark/batch/src/main/scala/com/tradesys/jobs/fx/ForeignExchangeDailyRates.scala
@@ -1,7 +1,9 @@
 package com.tradesys.jobs.fx
 
 import com.engine.processes.IProcessable
+import com.mongodb.spark.sql.toSparkSessionFunctions
 import com.tradesys.utils.properties.ApplicationProperties
+import com.tradesys.utils.sources.MongoDbService
 import org.apache.spark.sql.SparkSession
 
 class ForeignExchangeDailyRates extends IProcessable {
@@ -13,6 +15,12 @@ class ForeignExchangeDailyRates extends IProcessable {
     * @param properties property file with postgresql connection details
     */
   override def execute(session: SparkSession, properties: ApplicationProperties): Unit = {
-    println("hello job")
+    import org.apache.spark.sql.functions._
+    import session.implicits._
+
+    val mongoDbService = new MongoDbService(properties)
+    val mongoConfig = mongoDbService.createMongoReadConfig("fxrate")
+    val sourceData = session.loadFromMongoDB(mongoConfig)
+    sourceData.show(2, false)
   }
 }
--- spark/common/src/main/scala/com/tradesys/utils/cli/CliReader.scala
+++ spark/common/src/main/scala/com/tradesys/utils/cli/CliReader.scala
@@ -1,5 +1,6 @@
 package com.tradesys.utils.cli
 
+import java.nio.file.{Files, Paths}
 import org.rogach.scallop.{ScallopConf, ScallopOption}
 
 final case class CliObject(configPath: String, processName: String)
@@ -8,7 +9,28 @@ class CliReader(args: Array[String]) extends ScallopConf(args) {
   private val configPath: ScallopOption[String] = opt[String](name = "config", required = true)
   private val processName: ScallopOption[String] = opt[String](name = "process-name", required = false)
 
-  def createCliObject(): CliObject = CliObject(configPath.apply(), processName.getOrElse(""))
+  /**
+    * Creates CLI object
+    *
+    * @return CLI object
+    */
+  def createCliObject(): CliObject = {
+    val isConfigPathValid = validateConfigPath(configPath.apply())
+    if (!isConfigPathValid) {
+      throw new IllegalArgumentException("Config file: " + configPath.apply() + " not found")
+    }
+    CliObject(configPath.apply(), processName.getOrElse(""))
+  }
+
+  /**
+    * Validates configuration file path passed by user
+    *
+    * @param configPath configuration file path
+    * @return whether configuration file exists
+    */
+  private def validateConfigPath(configPath: String): Boolean = {
+    Files.exists(Paths.get(configPath))
+  }
 
   verify()
 }
