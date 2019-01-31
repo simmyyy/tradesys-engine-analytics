c75c7db1c70c88f6cd7879c2a1a5edac21fcf94c
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-01-31 22:55:23 +0100

    Preparign mongo data for processing v1

--- spark/batch/src/main/scala/com/tradesys/jobs/fx/ForeignExchangeDailyRates.scala
+++ spark/batch/src/main/scala/com/tradesys/jobs/fx/ForeignExchangeDailyRates.scala
@@ -21,6 +21,25 @@ class ForeignExchangeDailyRates extends IProcessable {
     val mongoDbService = new MongoDbService(properties)
     val mongoConfig = mongoDbService.createMongoReadConfig("fxrate")
     val sourceData = session.loadFromMongoDB(mongoConfig)
-    sourceData.show(2, false)
+    sourceData.printSchema()
+
+    val mappedDf = sourceData
+      .filter("dataprovider == 'ALPHAVANTAGE'")
+      .withColumn("startTimeDate", concat_ws("-", col("startTime.date.year"), col("startTime.date.month"), col("startTime.date.day")))
+      .withColumn("startTimeTime", concat_ws(":", col("startTime.time.hour"), col("startTime.time.minute"), col("startTime.time.second")))
+      .withColumn("startTime", concat_ws("T", col("startTimeDate"), col("startTimeTime")))
+      .withColumn("from_symbol", col("data._children.Meta Data._children.2__ From Symbol"))
+      .withColumn("to_symbol", col("data._children.Meta Data._children.3__ To Symbol"))
+      .filter("from_symbol is not null")
+      .select($"startTime", $"from_symbol", $"to_symbol", to_json($"data._children.Time Series FX (Daily)._children"))
+
+    mappedDf.printSchema()
+    mappedDf.show(10, truncate = false)
+
+    val collectedData = mappedDf.collectAsList()
+    println(collectedData.size())
+//    collectedData.forEach(ok => {
+//      println(ok.toString())
+//    })
   }
 }
